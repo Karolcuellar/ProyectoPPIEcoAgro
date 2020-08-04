16b9d6bd24798da4fa0ccfb846df2391e57c4934
MayraPosada1401 <mayravanessaposadaalvarez@gmail.com>
2020-08-04 18:09:06 -0500

    Se crea proyecto React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <tr className="tabla">Nombre</tr>
+          <td className="table-active">Karol Eliana Cuellar Narváez</td>
+          <td className="table-active">Mayra Vanessa Posada Alvarez</td>
+          <td className="table-active">Dahiana Restrepo Londoño</td>
+          <td className="table-active">Mariana Soto Maya</td>
+
+        </tr>
+        <tr>
+          <tr className="tabla">Correo</tr>
+
+          <td className="table-active">karoleliana2003@gmail.com</td>
+          <td className="table-active">mayravanessaposadaalvarez@gmail.com</td>
+          <td className="table-active">dahianarl113@gmail.com</td>
+          <td className="table-active">marianasotomaya@gmail.com</td>
+
+        </tr>
+        <tr>
+          <tr className="tabla">Tarjeta de Identidad</tr>
+          <td className="table-active">1083864859</td>
+          <td className="table-active">1088824206</td>
+          <td className="table-active">1015332035</td>
+          <td className="table-active">1000536751</td>
+
+        </tr>
+
+        <tr>
+          <tr className="tabla">Telefono</tr>
+          <td className="table-active">3107747306</td>
+          <td className="table-active">3005773510</td>
+          <td className="table-active">3194960925</td>
+          <td className="table-active">3113144328</td>
+        </tr>
+      </table>
     </div>
   );
 }
